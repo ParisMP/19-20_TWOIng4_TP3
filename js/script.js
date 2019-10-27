3fdc0974bc33f0f829b0854d4ffce4f1ec0ed857
ParisMP <36534560+ParisMP@users.noreply.github.com>
2019-10-27 18:26:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
-
 // Fonction appelée lors du click du bouton
-function start() {
-  // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
-  // Appel de la fonction fetchTodayForecast
+function start(city) {
 
-  apiWeather
-    .fetchTodayForecast()
-    .then(function(response) {
-      // Récupère la donnée d'une API
-      const data = response.data;
+    // Création de l'objet apiWeather
+    const apiWeather = new API_WEATHER(city);
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
-    })
-    .catch(function(error) {
-      // Affiche une erreur
-      console.error(error);
-    });
+    apiWeather
+        .getThreeDayForecast()
+        .then(function(response) {
+            // Récupère la donnée d'une API
+            const data = response.data.list;
+
+            for(let i=0; i<4; i++) {
+
+                // On récupère l'information principal
+                const main = data[i].weather[0].main;
+                const description = data[i].weather[0].description;
+                const icon = apiWeather.getHTMLElementFromIcon(data[i].weather[0].icon);
+                const temp = data[i].temp.day;
+
+                // Modifier le DOM
+                document.getElementById('day'+i+'-forecast-main').innerHTML = main;
+                document.getElementById('day'+i+'-forecast-more-info').innerHTML = description;
+                document.getElementById('icon'+i+'-weather-container').innerHTML = icon;
+                document.getElementById('day'+i+'-forecast-temp').innerHTML = `${temp}°C`;
+            }
+
+        })
+        .catch(function(error) {
+            // Affiche une erreur
+            console.error(error);
+            alert("ville non-trouvee, reessayer");
+        });
+}
+
+//Fonction appelée lors de l'actualisation de la ville
+function inputVille() {
+
+    //Récupérer la valeur de l'input
+    const cityInput = document.getElementById('city-input').value;
+
+    // Vérifier que l'input n'est pas vide
+    cityInput === "" ? alert("Veuillez indiquer une ville !") : start(cityInput);
 }
