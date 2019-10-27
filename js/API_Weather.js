9e1122d8d5a6fd1563f63921235914b762140712
ParisMP <36534560+ParisMP@users.noreply.github.com>
2019-10-27 18:25:47 +0100

    Update API_Weather.js

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -6,6 +6,8 @@ const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
+// Url API
+const API_URL2 = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
 
 class API_WEATHER{
@@ -19,12 +21,14 @@ class API_WEATHER{
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
-  fetchTodayForecast(){
+
+  getThreeDayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
-      crossdomain: true
-    })
+        .get(`${API_URL2}?q=${this.city}&units=metric&cnt=4&appid=${API_KEY}`, {
+          crossdomain: true
+        })
   }
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
